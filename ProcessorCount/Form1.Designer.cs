ba90e28f8166f501907fcc090b108debaebb2aa7
Archangel <djarchangel3737@gmail.com>
2017-09-01 13:12:21 +0300

    started project, some usefull info about PC

---
+++ ProcessorCount/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ProcessorCount
+{
+    partial class SysFormInfo
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SysInfo = new System.Windows.Forms.Button();
+            this.infoTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // SysInfo
+            // 
+            this.SysInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SysInfo.Location = new System.Drawing.Point(12, 12);
+            this.SysInfo.Name = "SysInfo";
+            this.SysInfo.Size = new System.Drawing.Size(196, 65);
+            this.SysInfo.TabIndex = 0;
+            this.SysInfo.Text = "Get System Info";
+            this.SysInfo.UseVisualStyleBackColor = true;
+            this.SysInfo.Click += new System.EventHandler(this.SysInfo_Click);
+            // 
+            // infoTextBox
+            // 
+            this.infoTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.infoTextBox.Location = new System.Drawing.Point(12, 83);
+            this.infoTextBox.Name = "infoTextBox";
+            this.infoTextBox.Size = new System.Drawing.Size(778, 594);
+            this.infoTextBox.TabIndex = 1;
+            this.infoTextBox.Text = "";
+            // 
+            // SysFormInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(802, 689);
+            this.Controls.Add(this.infoTextBox);
+            this.Controls.Add(this.SysInfo);
+            this.Name = "SysFormInfo";
+            this.Text = "System Info";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SysInfo;
+        private System.Windows.Forms.RichTextBox infoTextBox;
+    }
+}
+
---
+++ ProcessorCount/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace ProcessorCount
+{
+    public partial class SysFormInfo : Form
+    {
+        public SysFormInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void SysInfo_Click(object sender, EventArgs e)
+        {
+            infoTextBox.Text = "";
+            infoTextBox.Text += "Number of CPUs: " + System.Environment.ProcessorCount + Environment.NewLine;
+            infoTextBox.Text += "Is this process is 64-bit: " + System.Environment.Is64BitProcess + Environment.NewLine;
+            infoTextBox.Text += "Is this OS is 64-bit: " + System.Environment.Is64BitOperatingSystem + Environment.NewLine;
+            infoTextBox.Text += "OS version: " + System.Environment.OSVersion + Environment.NewLine;
+            infoTextBox.Text += "NetBIOS name of this machine: " + System.Environment.MachineName + Environment.NewLine;
+            infoTextBox.Text += "Pagefile size: " + System.Environment.SystemPageSize + Environment.NewLine;
+            infoTextBox.Text += "User Domain name: " + System.Environment.UserDomainName + Environment.NewLine;
+            infoTextBox.Text += "User Name: " + System.Environment.UserName + Environment.NewLine + 
+                "==========================================" + Environment.NewLine;
+
+            PerformanceCounter ramFree = new PerformanceCounter("Memory", "Available MBytes");
+            infoTextBox.Text += "Free RAM: " + ramFree.NextValue() + " MB" + Environment.NewLine;
+
+            ulong TotalRam = new Microsoft.VisualBasic.Devices.ComputerInfo().TotalPhysicalMemory;
+
+            infoTextBox.Text += "Total RAM: " + TotalRam / (1024 * 1024) + " MB" + Environment.NewLine + 
+                "==========================================" + Environment.NewLine;
+
+            infoTextBox.Text += "Drives INFO:" + Environment.NewLine + Environment.NewLine;
+
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+            foreach (DriveInfo d in allDrives)
+            {
+                infoTextBox.Text += "Drive " + d.Name + Environment.NewLine;
+                infoTextBox.Text += "Drive type: " + d.DriveType + Environment.NewLine;
+                if (d.IsReady == true)
+                {
+                    infoTextBox.Text += ">>  Volume label: " + d.VolumeLabel + Environment.NewLine;
+                    infoTextBox.Text += ">>  File system: " + d.DriveFormat + Environment.NewLine;
+                    infoTextBox.Text += 
+                        ">>  Available space to current user: " +
+                        d.AvailableFreeSpace + " bytes (" + d.AvailableFreeSpace/(1024*1024) + " Mb, " + 
+                        d.AvailableFreeSpace/(1024*1024*1024) + " Gb)" + Environment.NewLine;
+
+                    infoTextBox.Text += 
+                        ">>  Total available space: " +
+                        d.TotalFreeSpace + " bytes (" + d.TotalFreeSpace/(1024*1024) + " Mb, " + 
+                        d.TotalFreeSpace/(1024*1024*1024) + " Gb)" + Environment.NewLine;
+
+                    infoTextBox.Text += 
+                        ">>  Total size of drive: " +
+                        d.TotalSize + " bytes (" + d.TotalSize / (1024 * 1024) + " Mb, " + 
+                        d.TotalSize / (1024 * 1024 * 1024) + " Gb)" + Environment.NewLine + Environment.NewLine;
+                }
+            }
+
+        }
+    }
+}
